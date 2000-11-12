454be86102dd23e1d25157240affc81c928fdb36
bckfnn <bckfnn@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2000-11-12 22:51:37 +0000

    First version.

---
+++ htdocs/applets/links.h
@@ -0,0 +1,21 @@
+<!-- -*- html -*- -->
+<h3>Home</h3>
+<li><a href="../index.html">Overview</a>
+<li><a href="../license.html">License</a>
+<li><a href="../download.html">Jython 2.0</a>
+<li><a href="../install.html">Installing</a>
+<li><a href="../platform.html">JVM Compatibility</a>
+
+<h3>Applets</h3>
+<li><a href="index.html">Demos</a>
+<li><a href="button.html">ButtonDemo</a>
+<li><a href="checkbox.html">CheckboxDemo</a>
+<li><a href="choice.html">ChoiceDemo</a>
+<li><a href="label.html">LabelDemo</a>
+<li><a href="list.html">ListDemo</a>
+<li><a href="coordinates.html">CoordinatesDemo</a>
+<li><a href="converter.html">ConvertDemo</a>
+
+<h3>Applet Problems</h3>
+<li><a href="problems.html">Here's what to do</a>
+<li><a href="issues.html">Other applet issues</a>
---
+++ htdocs/links.h
@@ -0,0 +1,25 @@
+<!-- -*- html -*- -->
+<h3>Home</h3>
+<li><a href="./index.html">Overview</a>
+<li><a href="license.html">License</a>
+<li><a href="download.html">Jython 2.0</a>
+<li><a href="install.html">Installing</a>
+<li><a href="platform.html">JVM Compatibility</a>
+
+<h3>Resources</h3>
+<li><a href="applets/index.html">Applet Demos</a>
+<li><a href="http://sourceforge.net/mail/?group_id=12867">Mailling Lists (exit)</a>
+<li><a href="http://sourceforge.net/bugs/?group_id=12867">Bugs (exit)</a>
+<li><a href="users.html">Jython Users</a>
+
+<h3>Doc Quick Links</h3>
+<li><a href="docs/whatis.html">What is Jython</a>
+<li><a href="docs/index.html">All Documentation</a>
+<li><a href="docs/FAQ.html">FAQ</a>
+<li><a href="http://www.jpython.org/jpython-talk-1.ppt">JimH's Slides</a>
+<br>(PowerPoint, ~239KB)
+
+<h3>Exits</h3>
+<li><a href="http://www.python.org/">Python Language</a>
+<li><a href="http://www.python.org/psa">PSA</a>
+<li><a href="http://www.javasoft.com">Java Language</a>
