02348e31190d9e36b318282b66ea6e7b420a14d8
bckfnn <bckfnn@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2000-11-13 10:16:40 +0000

    First version.

---
+++ htdocs/docs/links.h
@@ -0,0 +1,32 @@
+<!-- -*- html -*- -->
+<h3>Home</h3>
+<li><a href="../index.html">Overview</a>
+<li><a href="../license.html">License</a>
+<li><a href="../download.html">Jython 2.0</a>
+<li><a href="../install.html">Installing</a>
+<li><a href="../platform.html">JVM Compatibility</a>
+
+<h3>Documentation</h3>
+<li><a href="index.html">Overview</a>
+<li><a href="whatis.html">Executive Summary</a>
+<li><a href="interpreter.html">Invoking Jython</a>
+<li><a href="registry.html">Jython Registry</a>
+<li><a href="embedding.html">Embedding</a>
+<li><a href="compile.html">Compiling Jython from source</a>
+
+<h3>Working with Java</h3>
+<li><a href="usejava.html">The Basics</a>
+<li><a href="properties.html">JavaBean Properties</a>
+<li><a href="jarray.html">Java arrays</a>
+<li><a href="subclassing.html">Subclassing</a>
+<li><a href="jythonc.html">Building applets, servlets, beans...</a>
+
+<h3>Python Docs (exits)</h3>
+<li><a href="http://www.python.org/doc/tut/tut.html">Python Tutorial</a>
+<li><a href="http://www.python.org/doc/lib/lib.html">Library Reference</a>
+
+<h3>Other</h3>
+<li><a href="differences.html">Jython vs. CPython</a>
+<li><a href="faq.html">Jython FAQ</a>
+<li><a href="http://www.geocrawler.com/archives/3/7017/2000/">List Archives</a> (exit)
+<LI><A HREF="http://www.python.org/workshops/1997-10/proceedings/hugunin.html">JPython paper</A> (exit)
