6d9f6712282fe4310a154569a2873681a34d8af7
otmarhumbel <otmarhumbel@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2005-10-19 21:19:16 +0000

    removed unused imports

--- installer/org/python/util/install/ReadmePage.java
+++ installer/org/python/util/install/ReadmePage.java
@@ -2,11 +2,6 @@ package org.python.util.install;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
