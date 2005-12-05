1c63f07f750f1e777a358a8c0e5459caa78b4efb
fwierzbicki <fwierzbicki@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2005-12-05 00:46:54 +0000

    Make the css for the left menu better.

--- sandbox/wierzbicki/mockup/go.sh
+++ sandbox/wierzbicki/mockup/go.sh
@@ -9,3 +9,4 @@ python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../c
 python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/books_articles.txt Project/books_articles.html
 python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/hacking.txt Project/hacking.html 
 python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/contributors.txt Project/contributors.html 
+python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/userdoc.txt Project/userdoc.html 
