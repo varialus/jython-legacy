d7308f84aa2136b2bbd8083cc4df4c05505a32aa
fwierzbicki <fwierzbicki@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2005-12-06 01:13:08 +0000

    Added contributors.

--- sandbox/wierzbicki/mockup/go.sh
+++ sandbox/wierzbicki/mockup/go.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Very crude -- need to automate this.
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/index.txt Project/index.html 
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/license.txt Project/license.html
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/news.txt Project/news.html
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/history.txt Project/history.html
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/faq.txt Project/faq.html
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/books_articles.txt Project/books_articles.html
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/hacking.txt Project/hacking.html 
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/contributors.txt Project/contributors.html 
-python ../../../docutils/tools/rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/userdoc.txt Project/userdoc.html 
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/index.txt Project/index.html 
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/license.txt Project/license.html
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/news.txt Project/news.html
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/history.txt Project/history.html
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/faq.txt Project/faq.html
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/books_articles.txt Project/books_articles.html
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/hacking.txt Project/hacking.html 
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/contributors.txt Project/contributors.html 
+rst2jysite.py --link-stylesheet --stylesheet=../css/html4css1.css Project/userdoc.txt Project/userdoc.html 
