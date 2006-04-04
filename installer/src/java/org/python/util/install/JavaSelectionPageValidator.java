7438f7d7bfb7fac643a2ee4392481b1f1d26a392
otmarhumbel <otmarhumbel@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2006-04-04 22:56:27 +0000

    do external java version checking only when needed

--- installer/src/java/org/python/util/install/JavaSelectionPageValidator.java
+++ installer/src/java/org/python/util/install/JavaSelectionPageValidator.java
@@ -16,11 +16,19 @@ public class JavaSelectionPageValidator extends AbstractWizardValidator {
     protected void validate() throws ValidationException {
         String directory = _page.getJavaHome().getText().trim(); // trim to be sure
         File javaHome = new File(directory);
-        JavaVersionInfo javaVersionInfo = Installation.getExternalJavaVersion(javaHome);
-        if (javaVersionInfo.getErrorCode() != Installation.NORMAL_RETURN) {
-            throw new ValidationException(javaVersionInfo.getReason());
+        JavaVersionInfo javaVersionInfo = new JavaVersionInfo();
+        String currentJavaHomeName = System.getProperty(JavaVersionTester.JAVA_HOME);
+        if (currentJavaHomeName.equals(javaHome.getAbsolutePath())) {
+            // no experiments if current java is selected
+            Installation.fillJavaVersionInfo(javaVersionInfo, System.getProperties());
+            javaHome = new File(currentJavaHomeName);
+        } else {
+            javaVersionInfo = Installation.getExternalJavaVersion(javaHome);
+            if (javaVersionInfo.getErrorCode() != Installation.NORMAL_RETURN) {
+                throw new ValidationException(javaVersionInfo.getReason());
+            }
         }
-        FrameInstaller.setTargetJavaHome(directory);
+        FrameInstaller.setTargetJavaHome(javaHome.getAbsolutePath());
         FrameInstaller.setJavaVersionInfo(javaVersionInfo);
     }
 
