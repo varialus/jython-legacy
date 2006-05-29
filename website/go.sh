6034c16f91d03e014c188facca987bb820f6394c
fwierzbicki <fwierzbicki@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2006-05-29 19:15:20 +0000

    Moving mockup website to top-level svn project as prep to going live.

---
+++ website/go.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#example command line to apply docutils to a single document.
+rst2jysite --link-stylesheet --stylesheet=../css/html4css1.css Project/userdoc.txt Project/userdoc.html
+
