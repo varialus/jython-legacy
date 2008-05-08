9bb69d99890c08b6a3b157b4a96a9113b90cee12
fwierzbicki <fwierzbicki@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2008-05-08 23:03:22 +0000

    Overwrite old output.

--- sandbox/wierzbicki/pydoclet/doclet.py
+++ sandbox/wierzbicki/pydoclet/doclet.py
@@ -7,19 +7,15 @@ class doclet(Processor):
         pass
 
     def process(self, root):
-        #for clazz in root.classes():
-        #    print clazz.name()
-        #    for method in clazz.methods():
-        #        print method.name()
-        print "***Packages***"
         for pac in root.specifiedPackages():
+            print "working..."
             path = pac.name().replace(".", "/")
             if not os.path.exists(path):
                 os.makedirs(path)
-            init = open(os.path.join(path, "__init__.py"), 'a')
+            init = open(os.path.join(path, "__init__.py"), 'w')
             
             for clazz in pac.allClasses():
-                print >> init, "class %s:" % clazz.name()
+                print >> init, "class %s:\n    '''%s'''" % (clazz.name(), clazz.commentText())
                 for method in clazz.methods():
-                    print >> init, "    def %s():\n        pass\n" % method.name()
+                    print >> init, "    def %s():\n        '''%s'''\n        pass\n" % (method.name(), method.commentText())
  
