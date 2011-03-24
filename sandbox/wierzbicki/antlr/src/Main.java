82b0ec371bcd8f1d85e8966044a0be90f1b752ed
fwierzbicki <fwierzbicki@79b11f02-f210-0410-99aa-fb189b3d1ac4>
2011-03-24 01:01:57 +0000

    Glob in Java to speed up grammar regression tests.

--- sandbox/wierzbicki/antlr/src/Main.java
+++ sandbox/wierzbicki/antlr/src/Main.java
@@ -6,24 +6,46 @@ import org.antlr.stringtemplate.language.*;
 public class Main {
     // override nextToken to set startPos (this seems too hard)
     public static class MyLexer extends PythonLexer {
-	public MyLexer(CharStream lexer) {
-	    super(lexer);
-	}
-	public Token nextToken() {
-	    startPos = getCharPositionInLine();
-	    return super.nextToken();
-	}
+        public MyLexer(CharStream lexer) {
+            super(lexer);
+        }
+        public Token nextToken() {
+            startPos = getCharPositionInLine();
+            return super.nextToken();
+        }
     }
 
     public static void main(String[] args) throws Exception {
-	CharStream input = new ANTLRFileStream(args[0]);
-	PythonLexer lexer = new MyLexer(input);
-	CommonTokenStream tokens = new CommonTokenStream(lexer);
-	tokens.discardOffChannelTokens(true);
-	PythonTokenSource indentedSource = new PythonTokenSource(tokens, "<test>");
-	tokens = new CommonTokenStream(indentedSource);
-	//System.out.println("tokens="+tokens.getTokens());
-	PythonParser parser = new PythonParser(tokens);
-	parser.file_input();
+        walk(new File(args[0]));
+    }
+
+    public static void walk(File dir) throws Exception {
+        String pattern = ".py";
+ 
+        File pyfiles[] = dir.listFiles();
+        if(pyfiles != null) {
+            for(int i=0; i<pyfiles.length; i++) {
+                if(pyfiles[i].isDirectory()) {
+                    walk(pyfiles[i]);
+                } else {
+                    if(pyfiles[i].getName().endsWith(pattern)) {
+                        run(pyfiles[i].getPath());
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static void run(String path) throws Exception {
+        System.out.println("parsing " + path);
+        CharStream input = new ANTLRFileStream(path);
+        PythonLexer lexer = new MyLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        tokens.discardOffChannelTokens(true);
+        PythonTokenSource indentedSource = new PythonTokenSource(tokens, "<test>");
+        tokens = new CommonTokenStream(indentedSource);
+        PythonParser parser = new PythonParser(tokens);
+        parser.file_input();
     }
 }
